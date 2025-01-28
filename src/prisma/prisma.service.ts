b4bb410d07f0c4ef3218f17ba315d15800d916a0
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-01-28 19:51:06 +0100

    Update prisma.service.ts

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@nestjs/common';
+// src/prisma/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  constructor() {
-    super();
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
+    await this.$connect(); // Connect to the database
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect(); // Disconnect on shutdown
   }
 }
