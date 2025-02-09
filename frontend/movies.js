6b4746a82ac597a64d1c12f5b07a2a290d58a9bd
arturr0 <119508496+arturr0@users.noreply.github.com>
2025-02-09 09:32:30 +0100

    Update movies.js

--- frontend/movies.js
+++ frontend/movies.js
@@ -404,7 +404,7 @@ document.addEventListener("userDataReady", () => {
 		const data = JSON.parse(event.data);
 		//console.log(`New ${data.type} update: ${data.title}`);
 		console.log(`data.querySenderID ${data.querySenderID} = userData.user.id ${userData.user.id} || (currentQuerry.type ${currentQuerry.type} !data.queryType ${data.queryType}  &currentQuerry.text ${currentQuerry.text} ! data.queryText ${data.queryText}`)
-		if (/*data.querySenderID == userData.user.id || */!(currentQuerry.type == data.queryType && currentQuerry.text == data.queryText)) return;
+		if (/*data.querySenderID == userData.user.id || */!(currentQuerry.type == data.queryType && currentQuerry.text.toLowerCase() == data.queryText.toLowerCase())) return;
 		//if (currentQuerry.type == data.queryType && currentQuerry.text == data.queryText) {
 			//if (currentQuerry.type != data.queryType && currentQuerry.text != data.queryText)
 			console.log("update", data);
